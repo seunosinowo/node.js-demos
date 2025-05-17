17dd036e81cbe28c660ab0ee8163e5450331413d
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-17 13:57:04 +0100

    Setting up and working with Modules

---
+++ add.js
@@ -0,0 +1,8 @@
+//Arrow functions
+const add = (a, b) => {
+    return a + b;
+}
+// const sum = add(2, 8)
+// console.log(sum)
+
+module.exports = add;
---
+++ index.js
@@ -0,0 +1,11 @@
+const addFn = require("./add")
+
+console.log("Hello from Index.js")
+
+
+//Modules export
+const sum = addFn(2,3)
+const sum1 = addFn(6,4)
+
+console.log(sum)
+console.log(sum1)
