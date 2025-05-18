f97c8967c2c2d419cff4b21e46f035a500055e99
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-18 17:38:38 +0100

    Module Caching

--- index.js
+++ index.js
@@ -10,5 +10,25 @@
 // console.log(sum)
 // console.log(sum1)
 
-require("./batman")
-require("./superman")
+// require("./batman")
+// require("./superman")
+
+// const superHero = require("./super-hero")
+// console.log(superHero.getName());
+
+// superHero.setName("Superman");
+// console.log(superHero.getName())
+ 
+// const newSuperHero = require("./super-hero");
+// console.log(newSuperHero.getName());
+
+const SuperHero = require("./super-hero");
+
+const batman = new SuperHero("Batman");
+console.log(batman.getName());
+batman.setName("Brune David")
+console.log(batman.getName())
+
+const superman = new SuperHero("superman");
+console.log(superman.getName())
+
