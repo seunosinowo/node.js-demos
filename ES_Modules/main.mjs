7e83b665802ae556990e020c9f04c55b1c3d032c
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-21 14:52:17 +0100

    Es Modules

---
+++ ES_Modules/main.mjs
@@ -0,0 +1,5 @@
+//ES Module (EcmaScript)
+import maths from "./maths-esm.mjs"
+
+console.log(maths.add(5, 3))
+console.log(maths.subtract(5, 3))
---
+++ ES_Modules/maths-esm.mjs
@@ -0,0 +1,20 @@
+//ES Module (EcmaScript)
+
+// const add = (a, b) => {
+//     return a + b;
+// };
+
+// export default add;
+
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+export default{
+    add,
+    subtract
+}
