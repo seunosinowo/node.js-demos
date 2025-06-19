89d5512d5e1e80e02b404035491c9ca7ce706c29
seunosinowo <oluwaseunpaul98@gmail.com>
2025-06-19 16:45:53 +0100

    Custom Packages

---
+++ my-custom-package/index.js
@@ -0,0 +1,5 @@
+function greet(name){
+    console.log(`Hello ${name},  welcome to Evolution`)
+}
+
+module.exports = greet;
---
+++ my-custom-package/upper.js
@@ -0,0 +1,8 @@
+const isUppercase = require("text-is-upper-case").isUpperCase
+
+function greet(name) {
+    console.log(`Hello ${name} welcome to my Home`)
+}
+
+greet("Seun")
+module.exports = greet;
